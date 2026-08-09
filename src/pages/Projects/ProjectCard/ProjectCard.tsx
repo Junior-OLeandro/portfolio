c06ef7635a653436a26e0a29dc7241238243d405
Junior Oliveira <junior.oleandro92@gmail.com>
2026-08-09 12:26:09 -0300

    cliques nos projetos

--- src/pages/Projects/ProjectCard/ProjectCard.tsx
+++ src/pages/Projects/ProjectCard/ProjectCard.tsx
@@ -8,18 +8,19 @@ interface Props {
 }
 
 export function ProjectCard({ project }: Props) {
-
     const [active, setActive] = useState(false);
 
     function handleCardClick() {
         if (window.matchMedia("(max-width: 1024px)").matches) {
             setActive(prev => !prev);
-
-            trackEvent("project_card_open", {
-                project_name: project.title,
-                project_slug: project.slug,
-            });
         }
+    }
+
+    function handleProjectView() {
+        trackEvent("project_view", {
+            project_name: project.title,
+            project_slug: project.slug,
+        });
     }
 
     return (
@@ -27,14 +28,12 @@ export function ProjectCard({ project }: Props) {
             className={`project ${project.size} ${project.category} ${active ? "active" : ""}`}
             onClick={handleCardClick}
         >
-
             <img
                 src={project.image}
                 alt={project.title}
             />
 
             <div className="overlay">
-
                 <h3>{project.title}</h3>
 
                 <p>{project.description}</p>
@@ -52,19 +51,13 @@ export function ProjectCard({ project }: Props) {
                     className="project-link w-40 mt-2 rounded-full p-2 text-center"
                     onClick={(event) => {
                         event.stopPropagation();
-
-                        trackEvent("project_view", {
-                            project_name: project.title,
-                            project_slug: project.slug,
-                        });
+                        handleProjectView();
                     }}
-                    onTouchStart={(event) => event.stopPropagation()}
                 >
                     Ver Projeto →
                 </Link>
-
             </div>
-
         </article>
     );
 }
+
